56e0a1277bc349d6e1afb6de18fe28d2255ce5a1
Edison Meneses <efmt86@gmail.com>
2025-06-12 07:00:24 -0500

    update

--- RDA3/aplicacion_basica/app.py
+++ RDA3/aplicacion_basica/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Bienvenido a Flask"
+
+@app.route('/base.html')
+def base_template():
+    return render_template('base.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
