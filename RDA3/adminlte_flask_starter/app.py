1046ad14ffd2db59c94e893e9648ed42de35873d
Edison Meneses <efmt86@gmail.com>
2025-06-12 09:57:14 -0500

    flask adminlte

---
+++ RDA3/adminlte_flask_starter/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home5():
+    return render_template("home.html")
+
+@app.route('/users')
+def user():
+    return render_template("usuarios.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
